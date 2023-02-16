13fe2fdc63ea14d90b9a5ec815d7fcab0656eae7
Alex Barongo <alexbarongo1@gmail.com>
2023-02-16 06:59:30 -0800

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - prints a message using printf function
+*Return: o
+*/
+
+int main(void)
+{
+char str[] = "with proper grammar, but the outcome is a piece of art,";
+
+printf("%s\n", str);
+return (0);
+}
