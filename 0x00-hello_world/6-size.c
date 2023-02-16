83204cc7fe41d1ccfde216ebb3bad2890cf5f177
Alex Barongo <alexbarongo1@gmail.com>
2023-02-16 08:28:56 -0800

    task advanced 100

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
 
 int main(void)
 {
-printf("Size of a char: %zu byte(s)\n", sizeof(char));
-printf("Size of an int: %zu byte(s)\n", sizeof(int));
-printf("Size of an long int: %zu byte(s)\n", sizeof(long));
-printf("Size of an long long int: %zu byte(s)\n", sizeof(long long));
-printf("Size of an float: %zu byte(s)\n", sizeof(float));
+printf("Size of a char: %ld byte(s)\n", sizeof(char));
+printf("Size of an int: %ld byte(s)\n", sizeof(int));
+printf("Size of an long int: %ld byte(s)\n", sizeof(long));
+printf("Size of an long long int: %ld byte(s)\n", sizeof(long long));
+printf("Size of an float: %ld byte(s)\n", sizeof(float));
 return (0);
 }
