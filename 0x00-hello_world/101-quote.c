77be60c686c3f5530f9b4373fa9af63a9c962234
Alex Barongo <105298694+alexbarongo@users.noreply.github.com>
2023-02-21 09:20:05 +0300

    task advanced 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
 * main - prints using getchar and putchar
@@ -7,6 +8,6 @@
 
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
