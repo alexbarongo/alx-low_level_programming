edf479e366f4bb45f71f2883ae780bd37ebfb74d
Alex Barongo <alexbarongo1@gmail.com>
2023-02-22 09:10:20 -0800

    completed task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer.
+*/
+
+void jack_bauer(void)
+{
+int hh, mm, h, m, a, b;
+char c;
+
+c = ':';
+for (a = 0; a <= 23; a++)
+{
+for (b = 0; b <= 59; b++)
+{
+hh = a / 10;
+h = a % 10;
+mm = b / 10;
+m = b % 10;
+
+_putchar(hh +'0');
+_putchar(h + '0');
+_putchar(c);
+_putchar(mm + '0');
+_putchar(m + '0');
+_putchar('\n');
+}
+}
+}
