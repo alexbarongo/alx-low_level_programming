73d1a6973989402d9c5b54318c562662d341b014
Alex Barongo <alexbarongo1@gmail.com>
2023-02-23 02:50:31 -0800

    completed task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* add - function that adds two integers and returns the result.
+*@n: parameter one
+*@m: parameter two
+* Return: Always 0.
+*/
+
+int add(int n, int m)
+{
+int result;
+result = n + m;
+
+return (result);
+}
