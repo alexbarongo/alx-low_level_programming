7f471f36b500fdd0b8d86d0c6dc801f1f1152ca0
Alex Barongo <alexbarongo1@gmail.com>
2023-02-24 06:48:31 -0800

    completed task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,11 +12,16 @@ int largest_number(int a, int b, int c)
 {
 int largest;
 
-if ((a == b && b == c && a == c) || (a == b && a > c) || (a == c && a > b))
+if ((a == b && b == c && a == c) ||
+(a == b && a > c) ||
+(a == c && a > b) ||
+(a > b && a > c))
 {
 largest = a;
 }
-else if ((b == a && b > c) || (b == c && b > a))
+else if ((b == a && b > c) ||
+(b == c && b > a) ||
+(b > a && b > c))
 {
 largest = b;
 }
