6a85c79662afa2ace79990d88a3044368d758c5b
Alex Barongo <alexbarongo1@gmail.com>
2023-02-24 08:13:34 -0800

    completed task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,26 +1,28 @@
 #include "main.h"
 /**
-*
+* _isupper - returns 1 if c is upper case
+* @c: parameter
+* Return: 1 uppercase 0 lowercase
 */
 
 int _isupper(int c)
 {
-  int i, j, r;
+int i, j, r;
 
-  for (i = 'A'; i <= 'Z'; i++)
-    {
-      if (c == i)
-      {
-        r = 1;
-      }
-    }
-  for (j = 'a'; j <= 'z'; j++)
-    {
-      if (c == j)
-      {
-        r = 0;
-      }
-    }
-  return (r);
+for (i = 'A'; i <= 'Z'; i++)
+{
+if (c == i)
+{
+r = 1;
+}
+}
+for (j = 'a'; j <= 'z'; j++)
+{
+if (c == j)
+{
+r = 0;
+}
+}
+return (r);
 }
 
