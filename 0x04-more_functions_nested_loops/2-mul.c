99c6354485a81e3f48f64c183fae26228f9a6c7d
Alex Barongo <alexbarongo1@gmail.com>
2023-02-24 23:55:04 -0800

    completed task 2

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
 * mul - function that multiplies two integers.
-** Return: returns the product 
+* @a: parameter 1
+* @b: parameter 2
+* Return: returns the product
 */
 
 int mul(int a, int b)
