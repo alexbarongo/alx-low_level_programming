1a09250e4720d24b2aa76d26a52e151a6cdbfa2e
Alex Barongo <alexbarongo1@gmail.com>
2023-02-27 06:35:17 -0800

    completed task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* _puts - function that prints a string, followed by a new line, to stdout.
+* @str: pointer parameter
+*/
+
+void _puts(char *str)
+{
+
+while
+(*str)
+{
+_putchar(*str++);
+}
+_putchar('\n');
+}
