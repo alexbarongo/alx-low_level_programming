4044d5e3b7461bca5db00c01d4ae70ec0435693a
Alex Barongo <alexbarongo1@gmail.com>
2023-03-02 09:09:14 -0800

    completed task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*
+*
+*
+*/
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int i, j;
+
+i = 0;
+j = 0;
+while (src[i++])
+{
+j++;
+}
+for (i = 0; src[i] && i < n; i++)
+{
+dest[i] = src[i];
+}
+for (i = j; i < n; i++)
+{
+dest[i] = '\0';
+}
+ 
+return (dest);
+}
