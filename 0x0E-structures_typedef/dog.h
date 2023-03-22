13bbb463053a02c0682e4a6cd996b20ab73449a4
Alex Barongo <alexbarongo1@gmail.com>
2023-03-22 02:00:17 -0700

    completed task the dog head file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Short description
+ * @name: name of the dog.
+ * @owner: owner of the dog. 
+ * @age: age of the dog.
+ *
+ * Description: structure that creates details about a dog.
+ */
+
+struct dog
+{
+  char *name;
+  char *owner;
+  float age;
+}
+
+#endif /* #ifndef _DOG_H_ */
