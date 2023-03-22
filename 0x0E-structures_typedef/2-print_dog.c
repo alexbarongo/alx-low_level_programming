80891c747d1e07fbcaeade2e0603cf423651a4df
Alex Barongo <alexbarongo1@gmail.com>
2023-03-22 05:26:44 -0700

    completed task 2-print_dog, a function that prints struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,42 +1,29 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
-/**
-* print_dog - a function that prints a struct dog
-* @d: The struct dog to be printed.
-*/
+#include <stdlib.h>
 
+/**
+ * print_dog - Prints a struct dog.
+ * @d: The struct dog to be printed.
+ */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-{
-return;
-}
+	if (d == NULL)
+		return;
 
-if (d->name == NULL)
-{
-printf("Name: (nil)\n");
-}
-else
-{
-printf("Name: %s\n", d->name);
-}
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
 
-if (d->age == NULL)
-{
-printf("Age: (nil)\n");
-}
-else
-{
-printf("Age: %s\n", d->age);
-}
+	if (d->age < 0)
+		printf("Age: (nil)\n");
+	else
+		printf("Age: %f\n", d->age);
 
-if (d->owner == NULL)
-{
-printf("Owner: (nil)\n");
-}
-else
-{
-printf("Owner: %s\n", d->owner);
-}
+	if (d->owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+
 }
